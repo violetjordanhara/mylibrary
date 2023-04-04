3d253fe8e3ecfc61f544dc49839e92a10cfa6f7e
vi <vjordanhara@gmail.com>
2023-04-04 11:28:54 +0100

    changed tables to cards, organised js

--- odinproject_library/library.js
+++ odinproject_library/library.js
@@ -1,89 +1,106 @@
-/* All of your book objects are going to be stored in a simple array, 
-so add a function to the script (not the constructor) that can take user’s input and 
-store the new book objects into an array. Your code should look something like this:
 let myLibrary = [];
-
-function Book() {
-  // the constructor...
-}
-
-function addBookToLibrary() {
-  // do stuff here
-}*/
-
-let myLibrary = [];
-
+let container = document.querySelector(`#container`)
 //book constructor
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, libraryIndex){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.libraryIndex = 0
 }
-//prompt user for each property, add it to myLibrary
+//take input from form, add new book to myLibrary, add it to the table
 function addBookToLibrary() {
-   let addBook = new Book(prompt("Title of new book"), prompt("Author of new book"),prompt("Page count of new book"),prompt("Read?"))
+   let addBook = new Book(document.getElementById('title').value, 
+                        document.getElementById('author').value,
+                        document.getElementById('pages').value,
+                        document.getElementById('read').value);
     myLibrary.push(addBook);
+
     console.log(myLibrary)
+    createBookDisplay()
 };
-addBookToLibrary()
 
-/*Write a function that loops through the array and displays each book on the page. 
-You can display them in some sort of table, or each on their own “card”. 
-It might help for now to manually add a few books to your array so you can see the display. */
-
-//three sample books to add to array
-const bookOne = {
-    title: "The Waves", 
-    author: "Virginia Woolf", 
-    pages: 300, 
-    read: "read"
-}
-const bookTwo ={
-    title:"Do Androids Dream of Electric Sheep", 
-    author:"Philip K. Dick", 
-    pages: 300, 
-    read: "read"
-}
-const bookThree = {
-    title: "The Sound of Waves", 
-    author: "Yukio Mishima", 
-    pages: 300, 
-    read: "not read"
-}
-myLibrary.push(bookOne, bookTwo, bookThree)
-console.log(myLibrary)
-//function that loops through array and inputs each item to the table
-function bookDisplay(){
+//function that loops through array and inputs each item to DOM
+function createBookDisplay(){
+//clear previous entries
+    container.replaceChildren();
     for (let i=0; i<myLibrary.length;i++){
-        var table = document.getElementById("books");
-        var row = table.insertRow(-1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        cell1.innerHTML = `${myLibrary[i].title}`;
-        cell2.innerHTML = `${myLibrary[i].author}`;
-        cell3.innerHTML = `${myLibrary[i].pages}`;
-        cell4.innerHTML = `${myLibrary[i].read}`;
+        //give it a library index to have attributes corresponding to dom
+        let currentBook = myLibrary[i];
+	    currentBook.libraryIndex = i;
+
+        let libraryCard = document.createElement('div');
+	    libraryCard.classList.add(`libraryCard`);
+	    libraryCard.setAttribute(`data-deleteIndex`, currentBook.libraryIndex);
+	    container.appendChild(libraryCard);
+
+        let bookTitle = document.createElement('h1');
+	    bookTitle.textContent = currentBook.title;
+	    libraryCard.appendChild(bookTitle);
+
+        let bookAuthor = document.createElement('div')
+        bookAuthor.textContent = currentBook.author;
+        libraryCard.appendChild(bookAuthor)
+
+        let bookPages = document.createElement('div')
+        bookPages.textContent = currentBook.pages;
+        libraryCard.appendChild(bookPages)
+
+        let bookRead = document.createElement('div')
+        bookRead.textContent = currentBook.read;
+        libraryCard.appendChild(bookRead)
+    
+    // add delete button, set attribute to correspond to the library index(i)
+	let bookDeleteButton = document.createElement(`button`);
+	bookDeleteButton.innerHTML = `Delete this book`;
+	bookDeleteButton.id = `deleteBook`;
+	bookDeleteButton.setAttribute(`data-buttonDeleteIndex`, currentBook.libraryIndex);
+	libraryCard.appendChild(bookDeleteButton);
     }
+    
 }
-bookDisplay();
-/*
-Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: 
-author, title, number of pages, whether it’s been read and anything else you might want. 
-You will most likely encounter an issue where submitting your form will not do what you expect it to do. 
-That’s because the submit input tries to send the data to a server by default. 
-If you’ve done the bonus section for the calculator assignment, you might be familiar with event.preventDefault();. 
-Read up on the event.preventDefault documentation again and see how you can solve this issue!
-*/
+createBookDisplay();
+
 
 //function displays the form on clicking the "new book" button
 function makeForm(){
     var x = document.getElementById("bookForm");
     x.style.display = "block";
 }
-
+//takes user input, adds new object in array. page refreshes and array is looped over
 function manageSubmit(){
     event.preventDefault();
+    addBookToLibrary();
+   
+}
+
+/*Add a button on each book’s display to remove the book from the library.
+You will need to associate your DOM elements with the actual book objects in some way. 
+One easy solution is giving them a data-attribute that corresponds to the index of the library array. */
+//create buttons for each row
+
+function resetLibraryDisplay() {
+    for (i = 0; i < myLibrary.length; i++) {
+        let libraryCards = container.querySelectorAll('div');
+    
+        libraryCards.forEach(libraryCard => { 
+			libraryCard.remove();
+    });
+    }
+}
+
+
+function deleteBook(indexToDelete) {
+    let libraryCards = container.querySelectorAll('div');
+    libraryCards.forEach(libraryCard => { 
+		if (indexToDelete === libraryCard.getAttribute(`data-deleteIndex`)) {
+			this.libraryCard.remove();
+		}
+    });
+	
+	resetLibraryDisplay();
+	console.log({myLibrary});
+	myLibrary.splice(indexToDelete, 1);
+	console.log({myLibrary});
+	createBookDisplay();
 }
